831736f13737aba251b0948a111612a9ccd5457b
anhdangh <anhdh.hut@gmail.com>
2018-03-24 23:43:18 +0700

    event thumbnail

---
+++ app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.compenent';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [EventsAppComponent, EventsListComponent, EventThumbnailComponent],
+    bootstrap:[EventsAppComponent]
+})
+export class AppModule {
+
+}
